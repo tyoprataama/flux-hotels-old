c296eb610911f89a75a96a557e6aa488d33b0fa3
tyoprataama <tyoprataama004@gmail.com>
2023-12-26 12:48:46 +0700

    build: create footer sections

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,9 +1,20 @@
+import Link from "next/link";
+
 const Footer = () => {
   return (
-    <div>
-      This is Footer
-    </div>
-  )
-}
+    <footer className="mt-16">
+      <div className="flex container mx-auto px-4 py-4 justify-center">
+        <Link
+          href="/"
+          className="hover:-translate-y-2 duration-500 transition-all"
+        >
+          <span className="font-bold text-blue-600">Flux</span>
+          tours
+        </Link>
+        <div className="ml-3">Made with 💖 by Tyo</div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
