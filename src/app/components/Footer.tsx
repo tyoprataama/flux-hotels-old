64d1b5924ec74ea553a62c7a743e1728d934e977
tyoprataama <tyoprataama004@gmail.com>
2023-12-26 13:12:15 +0700

    refactor: footer sections

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,15 +3,17 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <footer className="mt-16">
-      <div className="flex container mx-auto px-4 py-4 justify-center">
-        <Link
-          href="/"
-          className="hover:-translate-y-2 duration-500 transition-all"
-        >
-          <span className="font-bold text-blue-600">Flux</span>
-          hotels
-        </Link>
-        <div className="ml-3">Made with 💖 by Tyo</div>
+      <div className="bg-slate-300 h-10 w-full bottom-0 left-0">
+        <div className="flex container mx-auto px-4 py-2 justify-center">
+          <Link
+            href="/"
+            className="hover:-translate-y-2 duration-500 transition-all"
+          >
+            <span className="font-bold text-blue-600">Flux</span>
+            hotels
+          </Link>
+          <div className="ml-3">Made with 💖 by Tyo</div>
+        </div>
       </div>
     </footer>
   );
