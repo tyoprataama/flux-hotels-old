5f332000bc538ee925a63d97710221b8d6e19b4b
tyoprataama <tyoprataama004@gmail.com>
2023-12-26 12:30:50 +0700

    build: create header sections

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,9 +1,31 @@
+import Link from "next/link";
+
 const Header = () => {
   return (
-    <div>
-      This is Header
-    </div>
-  )
-}
+    <header className="py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between">
+      <div className="flex items-center w-full md:2/3">
+        <Link href="/"> <span className="font-bold text-blue-600">Flux</span>hotels</Link>
+        <ul className="flex items-center ml-5">
+          <li className="flex items-center cursor-pointer">
+            <Link href="/user">Icons</Link>
+          </li>
+          <li className="ml-2 cursor-pointer">Icons</li>
+        </ul>
+      </div>
 
-export default Header
+      <ul className="flex items-center justify-between w-full md:w-1/3 mt-4">
+        <li className="hover:-translate-y-2 duration-500 transition-all cursor-pointer">
+          <Link href="/">Home</Link>
+        </li>
+        <li className="hover:-translate-y-2 duration-500 transition-all cursor-pointer">
+          <Link href="/">Rooms</Link>
+        </li>
+        <li className="hover:-translate-y-2 duration-500 transition-all cursor-pointer">
+          <Link href="/">Contact</Link>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
