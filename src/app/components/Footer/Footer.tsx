24dd31fce95eed5dbc4da643f9a7c8d62c65156c
tyoprataama <tyoprataama004@gmail.com>
2023-12-27 17:10:28 +0700

    refactor: color styling

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -3,16 +3,15 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <footer className="mt-16">
-      <div className="bg-slate-300 h-10 w-full bottom-0 left-0">
+      <div className="bg-gray-300 h-10 w-full bottom-0 left-0 dark:bg-black">
         <div className="flex container mx-auto px-4 py-2 justify-center">
-          <Link
-            href="/"
-            className="hover:-translate-y-2 duration-500 transition-all"
-          >
+          <div className="text-gray-500 dark:text-gray-300">
             <span className="font-bold text-blue-600">Flux</span>
             hotels
-          </Link>
-          <div className="ml-3">Made with 💖 by Tyo</div>
+          </div>
+          <div className="ml-3 text-gray-500 dark:text-gray-300">
+            Made with 💖 by Tyo
+          </div>
         </div>
       </div>
     </footer>
